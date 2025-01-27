cdabe78dd8461c059064d01d0083c84782e69d14
Jonathan Barnett <8984719+jbcbdse@users.noreply.github.com>
2025-01-27 14:31:33 -0600

    fix: Allow tool handler to return a promise

--- packages/core/src/base-tool.ts
+++ packages/core/src/base-tool.ts
@@ -39,5 +39,5 @@ export abstract class BaseTool implements ITool {
   public abstract handler(
     params: z.infer<typeof this.schema>,
     context: ChatAgentContext,
-  ): string;
+  ): string | Promise<string>;
 }
